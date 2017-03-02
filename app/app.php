91a27d8792caac68eb5422c4700643a18ff8e29a
Benjamin T. Seaver and Xia Amendolara <benjamin@>
2017-03-02 08:27:15 -0800

    Add initial Silex framework (app/, web/)

---
+++ app/app.php
@@ -0,0 +1,82 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    // require_once __DIR__."/../src/Stylist.php";
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $server = 'mysql:host=localhost:8889;dbname=library';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    // Book routes
+
+    $app->get("/", function() use ($app) {
+        return 'hello library';
+        // return $app['twig']->render('stylists.html.twig',
+        //     array('edit_stylist' => new Stylist, 'stylists' => Stylist::getAll())
+        // );
+    });
+
+    $app->post("/post/book", function() use ($app) {
+        return 'To do';
+
+        // $stylist = new Stylist($_POST['stylist_name'], $_POST['stylist_contact_info']);
+        // $stylist->save();
+        //
+        // return $app['twig']->render('stylists.html.twig',
+        //     array('edit_stylist' => new Stylist, 'stylists' => Stylist::getAll())
+        // );
+    });
+
+    $app->get("/get/book/{id}/edit", function($id) use ($app) {
+        return 'To do';
+        // $stylist = Stylist::findById($id);
+        //
+        // return $app['twig']->render('stylists.html.twig',
+        //     array('edit_stylist' => $stylist, 'stylists' => Stylist::getAll())
+        // );
+    });
+
+    $app->patch("/patch/book/{id}", function($id) use ($app) {
+        return 'To do';
+        // $stylist = Stylist::findById($id);
+        // $stylist->update($_POST['stylist_name'], $_POST['stylist_contact_info']);
+        //
+        // return $app['twig']->render('stylists.html.twig',
+        //     array('edit_stylist' => new Stylist, 'stylists' => Stylist::getAll())
+        // );
+    });
+
+    $app->delete("/delete/book/{id}", function($id) use ($app) {
+        return 'To do';
+        // $stylist = Stylist::findById($id);
+        // $stylist->delete();
+        //
+        // return $app['twig']->render('stylists.html.twig',
+        //     array('edit_stylist' => new Stylist, 'stylists' => Stylist::getAll())
+        // );
+    });
+
+    $app->delete("/delete/books", function() use ($app) {
+        return 'To do';
+        // Client::deleteAll();
+        // Stylist::deleteAll();
+        //
+        // return $app['twig']->render('stylists.html.twig',
+        //     array('edit_stylist' => new Stylist, 'stylists' => Stylist::getAll())
+        // );
+    });
+
+
+    return $app;
+?>
